c28779b537cf8ed20d38dfb5a2636841c9198988
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-11-11 17:25:23 +0100

    Added logic to CountryDataViewModel.kt

--- app/src/main/java/com/olisemeka/countryviewer/data/repository/CountryDataRepository.kt
+++ app/src/main/java/com/olisemeka/countryviewer/data/repository/CountryDataRepository.kt
@@ -1,4 +1,7 @@
 package com.olisemeka.countryviewer.data.repository
 
+import com.olisemeka.countryviewer.api.RetrofitInstance
+
 class CountryDataRepository {
+    suspend fun getCountryDataResults() = RetrofitInstance.api.getCountryDataResults()
 }
--- app/src/main/java/com/olisemeka/countryviewer/ui/CountryDataViewModel.kt
+++ app/src/main/java/com/olisemeka/countryviewer/ui/CountryDataViewModel.kt
@@ -1,6 +1,27 @@
 package com.olisemeka.countryviewer.ui
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.olisemeka.countryviewer.data.model.CountryData
+import com.olisemeka.countryviewer.data.repository.CountryDataRepository
+import com.olisemeka.countryviewer.util.Resource
+import kotlinx.coroutines.launch
 
-class CountryDataViewModel: ViewModel() {
+class CountryDataViewModel(val countryDataRepository: CountryDataRepository): ViewModel() {
+    init{
+        getCountryDataResults()
+    }
+
+    private var _countryDataResults = MutableLiveData<Resource<CountryData>>()
+    val countryDataResults: LiveData<Resource<CountryData>> get() = _countryDataResults
+
+    private fun getCountryDataResults() = viewModelScope.launch {
+        val response = countryDataRepository.getCountryDataResults()
+    }
+}
+
+
+class CountryDataViewModelProviderFactory(val countryDataRepository: CountryDataRepository): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return CountryDataViewModel(countryDataRepository) as T
+    }
 }
