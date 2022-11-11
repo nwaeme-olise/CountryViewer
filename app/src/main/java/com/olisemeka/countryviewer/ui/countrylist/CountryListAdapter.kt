1fd5b436244e219dac4cf937de59b2e9f4b22fe1
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-11-11 16:34:23 +0100

    Made changes to CountryListAdapter.kt

--- app/src/main/java/com/olisemeka/countryviewer/ui/countrylist/CountryListAdapter.kt
+++ app/src/main/java/com/olisemeka/countryviewer/ui/countrylist/CountryListAdapter.kt
@@ -1,34 +1,53 @@
 package com.olisemeka.countryviewer.ui.countrylist
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.olisemeka.countryviewer.R
+import com.olisemeka.countryviewer.data.model.CountryData
+import com.olisemeka.countryviewer.databinding.ListItemCountryBinding
 
-class CountryListAdapter(private val context: Context): ListAdapter<Any, CountryListAdapter.ViewHolder>(CountryDiffCallback) {
+class CountryListAdapter(private val context: Context): ListAdapter<CountryData, CountryListAdapter.ViewHolder>(CountryDiffCallback) {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
+    class ViewHolder(val binding: ListItemCountryBinding) : RecyclerView.ViewHolder(binding.root){
+        val ivCountry = binding.ivCountry
+        val tvCountry = binding.tvCountry
+        val tvCapital = binding.tvCapital
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(ListItemCountryBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.apply{
+            val country = getItem(position)
+            tvCountry.text = country.name.official
+            tvCapital.text = country.capital[0]
+            Glide.with(context)
+                .load(country.flags.svg)
+                .centerCrop()
+                .transform(RoundedCorners(20))
+                .into(ivCountry)
+        }
     }
 }
 
-object CountryDiffCallback: DiffUtil.ItemCallback<Any>() {
-    override fun areItemsTheSame(oldItem: Any, newItem: Any): Boolean {
-        TODO("Not yet implemented")
+object CountryDiffCallback: DiffUtil.ItemCallback<CountryData>() {
+    override fun areItemsTheSame(oldItem: CountryData, newItem: CountryData): Boolean {
+        return oldItem.name.official == newItem.name.official
     }
 
-    override fun areContentsTheSame(oldItem: Any, newItem: Any): Boolean {
-        TODO("Not yet implemented")
+    override fun areContentsTheSame(oldItem: CountryData, newItem: CountryData): Boolean {
+        return oldItem == newItem
     }
 
 }
